27f5412713fd16284f7b8b0790225e26ce560abd
BBlyde <benjaminblondey@orange.fr>
2024-10-25 00:41:44 +0200

    fix affichage tableau vide & setup curl

--- index.php
+++ index.php
@@ -1,42 +1,36 @@
+<meta http-equiv="refresh" content="60" />
 <style>
     <?php include 'style.css'; ?>
 </style>
 
-<?php
-// MSF
-// $url = "https://paceman.gg/api/get-event-completions?eventId=6713bb5daaa1ede5c2d980d3&usePoints=false";
-// TEST
-$url = "https://paceman.gg/api/get-event-completions?eventId=66d9e28f3aa05cdeb5c8bbec&usePoints=false";
-$raw = file_get_contents($url);
-$jsonLeaderbaord = json_decode($raw, true);
 
+<?php
+// Retrieve api endpoint datas
+// $url = "https://paceman.gg/api/get-event-completions?eventId=6713bb5daaa1ede5c2d980d3&usePoints=false"; // Msf Id
+$url = "https://paceman.gg/api/get-event-completions?eventId=66d9e28f3aa05cdeb5c8bbec&usePoints=false"; // Exemple Id
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_VERBOSE, true);
+
+$raw = curl_exec($ch);
+$jsonLeaderboard = json_decode($raw, true);
+curl_close($ch);
+
+// Trim json to keep only usefull data
 $leaderboard = [];
-foreach ($jsonLeaderbaord as $playerData) {
-    // var_dump($playerData);
+foreach ($jsonLeaderboard as $playerData) {
     $pb = $playerData['time'];
     $pb_minutes = floor($pb / (60 * 1000));
     $pb_seconds = floor(($pb % (60 * 1000)) / 1000);
 
     array_push($leaderboard, [$playerData["nickname"], $pb_minutes, $pb_seconds]);
 }
-
-usort($leaderboard, function ($a, $b) {
-    if ($a[1] > $b[1]) {
-        return 1;
-    } elseif ($a[1] < $b[1]) {
-        return -1;
-    } else {
-        if ($a[2] > $b[2]) {
-            return 1;
-        } elseif ($a[2] < $b[2]) {
-            return -1;
-        } else {
-            return 0;
-        }
-    }
-});
 ?>
 
+<!-- Main content -->
 <body>
     <div class="content-main">
         <div class="podium">
@@ -47,11 +41,11 @@ usort($leaderboard, function ($a, $b) {
                     <div class="skull">
                         <?php
                         if ($i == 0) {
-                            echo isset($leaderboard[$i]) ? '<img id="skull-first" src="https://mc-heads.net/avatar/' . $leaderboard[$i][0] . '">' : '<img id="skull-first" src="https://mc-heads.net/avatar/MHF_Question"';
+                            echo isset($leaderboard[$i]) ? '<img id="skull-first" src="https://mc-heads.net/avatar/' . $leaderboard[$i][0] . '">' : '<img id="skull-first" src="https://mc-heads.net/avatar/MHF_Question">';
                         } else if ($i == 1) {
-                            echo isset($leaderboard[$i]) ? '<img id="skull-second" src="https://mc-heads.net/avatar/' . $leaderboard[$i][0] . '">' : '<img id="skull-first" src="https://mc-heads.net/avatar/MHF_Question"';
+                            echo isset($leaderboard[$i]) ? '<img id="skull-second" src="https://mc-heads.net/avatar/' . $leaderboard[$i][0] . '">' : '<img id="skull-second" src="https://mc-heads.net/avatar/MHF_Question">';
                         } else {
-                            echo isset($leaderboard[$i]) ? '<img id="skull-third" src="https://mc-heads.net/avatar/' . $leaderboard[$i][0] . '">' : '<img id="skull-first" src="https://mc-heads.net/avatar/MHF_Question"';
+                            echo isset($leaderboard[$i]) ? '<img id="skull-third" src="https://mc-heads.net/avatar/' . $leaderboard[$i][0] . '">' : '<img id="skull-third" src="https://mc-heads.net/avatar/MHF_Question">';
                         }
                         ?>
                     </div>
@@ -70,50 +64,63 @@ usort($leaderboard, function ($a, $b) {
         </div>
 
         <div class="container">
-            <div id="tab-1" class="content active tab">
-                <?php
-                for ($i = 4; $i < 9; $i++) {
-                    if (isset($leaderboard[$i])) {
-                        echo '<span id="lb-' . $i . '">' . $leaderboard[$i - 1][0] . ' : ' . $leaderboard[$i - 1][1] . ":" . $leaderboard[$i - 1][2] . '</span>';
-                    } else {
-                        echo '<span id="lb-' . $i . '"></span>';
-                    }
+            <?php
+            if (isset($leaderboard[$i])) {
+                echo '<div id="tab-1" class="content valid active tab">';
+            } else {
+                echo '<div id="tab-1" class="content active tab">';
+            };
+            for ($i = 4; $i < 9; $i++) {
+                if (isset($leaderboard[$i-1])) {
+                    echo '<span id="lb-' . $i . '">' . $i . '. ' . $leaderboard[$i - 1][0] . ' : ' . $leaderboard[$i - 1][1] . ":" . $leaderboard[$i - 1][2] . '</span>';
+                } else {
+                    echo '<span id="lb-' . $i . '"></span>';
                 }
-                ?>
-            </div>
+            }
+            echo '</div>';
+            ?>
 
-            <div id="tab-2" class="content tab">
-                <?php
-                for ($i = 9; $i < 14; $i++) {
-                    if (isset($leaderboard[$i])) {
-                        echo '<span id="lb-' . $i . '">' . $leaderboard[$i - 1][0] . ' : ' . $leaderboard[$i - 1][1] . ":" . $leaderboard[$i - 1][2] . '</span>';
-                    } else {
-                        echo '<span id="lb-' . $i . '"></span>';
-                    }
+            <?php
+            if (isset($leaderboard[$i-1])) {
+                echo '<div id="tab-2" class="content valid tab">';
+            } else {
+                echo '<div id="tab-2" class="content tab">';
+            };
+            for ($i = 9; $i < 14; $i++) {
+                if (isset($leaderboard[$i-1])) {
+                    echo '<span id="lb-' . $i . '">' . $i . '. ' . $leaderboard[$i - 1][0] . ' : ' . $leaderboard[$i - 1][1] . ":" . $leaderboard[$i - 1][2] . '</span>';
+                } else {
+                    echo '<span id="lb-' . $i . '"></span>';
                 }
-                ?>
-            </div>
+            }
+            echo '</div>';
+            ?>
 
-            <div id="tab-3" class="content tab">
-                <?php
-                for ($i = 14; $i < 19; $i++) {
-                    if (isset($leaderboard[$i])) {
-                        echo '<span id="lb-' . $i . '">' . $leaderboard[$i - 1][0] . ' : ' . $leaderboard[$i - 1][1] . ":" . $leaderboard[$i - 1][2] . '</span>';
-                    } else {
-                        echo '<span id="lb-' . $i . '"></span>';
-                    }
+            <?php
+            if (isset($leaderboard[$i-1])) {
+                echo '<div id="tab-3" class="content valid tab">';
+            } else {
+                echo '<div id="tab-3" class="content tab">';
+            };
+            for ($i = 14; $i < 19; $i++) {
+                if (isset($leaderboard[$i-1])) {
+                    echo '<span id="lb-' . $i . '">' . $i . '. ' . $leaderboard[$i - 1][0] . ' : ' . $leaderboard[$i - 1][1] . ":" . $leaderboard[$i - 1][2] . '</span>';
+                } else {
+                    echo '<span id="lb-' . $i . '"></span>';
                 }
-                ?>
-            </div>
+            }
+            echo '</div>';
+            ?>
         </div>
 
         <script>
             let currentIndex = 0;
-            const divs = document.querySelectorAll('.content');
+            const divs = document.querySelectorAll('.valid');
 
             function showNextDiv() {
                 divs[currentIndex].classList.remove('active');
                 currentIndex = (currentIndex + 1) % divs.length;
+                console.log(divs.length);
                 divs[currentIndex].classList.add('active');
             }
             setInterval(showNextDiv, 2000);
